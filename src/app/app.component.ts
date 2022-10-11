10c7a19eaef2f149a59b5360761ea4a82d6d3a9f
Nabil Hamza <nabil.hamza@atos.net>
2022-10-11 10:48:56 +0200

    Retrieve Data with HttpClient - Get Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
+interface Entry{
+id: string;
+title:string;
+author:string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private httpClient:HttpClient) {
+  }
   title = 'AngularKeyclock';
+
+  ngOnInit(){
+this.httpClient.get('http://localhost:3000/posts').subscribe(data=>{
+  let data1 = <Entry[]>data;
+  console.log(data1);
+  console.log(data1[0].title);
+  this.title=data1[0].title;
+})
+  }
 }
