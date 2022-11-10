652896604e0be6955282e78bec14fbbbcf0f475b
Nabil Hamza <nabil.hamza@atos.net>
2022-11-10 08:35:16 +0100

    update docker-compose file

--- keycloack_config/init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sleep 1&&java -jar /config/keycloak-config-cli-19.0.1.jar --import.files.locations=/config/keycloak_config.json --keycloak.url=http://localhost:8080 --keycloak.ssl-verify=true --keycloak.user=admin --keycloak.password=admin --keycloak.availability-check.timeout=120 --keycloak.connect-timeout=10 
